597f2d1a3b2bb93d28bcaeed49ca64cc4795cf7f
nginnever <ginneversource@gmail.com>
2016-08-09 21:46:21 -0700

    find proper address storage

---
+++ contracts/FileswarmManager.sol
@@ -0,0 +1,32 @@
+contract FileswarmManager {
+  address owner;
+  mapping (address => address[]) [] files;
+  
+  modifier isOwner(bytes32 name)
+  {
+    if (msg.sender != owner) throw;
+    _
+  }
+
+  function FileswarmManager() {
+    owner = msg.sender;
+  }
+  
+  function upload(uint TTL) returns (bool res) {
+    address f = new File(msg.sender, msg.value);
+    address[] temp = files[msg.sender];
+    files[msg.sender].push(f);
+    return true;
+  }
+  
+}
+
+contract File {
+  address owner;
+  uint balance;
+  
+  function File(address s, uint v) {
+    owner = s;
+    balance = v;
+  }
+}
