c601738f27c2371a04e53124dabd7b20dbd90965
nginnever <ginneversource@gmail.com>
2016-08-11 00:30:00 -0700

    needs testing

--- contracts/FileswarmManager.sol
+++ contracts/FileswarmManager.sol
@@ -1,7 +1,7 @@
 contract FileswarmManager {
-  address owner;
-  address[] files;
-  mapping (address => address[]) seeders;
+  address public owner;
+  address[] public files;
+  mapping (address => string[]) public seeders;
 
   modifier isSeeding(address x)
   {
@@ -13,67 +13,94 @@ contract FileswarmManager {
     owner = msg.sender;
   }
   
+  function getFilesCount() public constant returns(uint) {
+    return files.length;
+  }
+  
   function upload(string hash1, string hash2) returns(bool res) {
     address f = new File(msg.sender, msg.value, hash1, hash2);
     files.push(f);
     return true;
   }
   
-  function seed(address ifile) isSeeding(ifile) returns(bool res) {
-    seeders[msg.sender].push(ifile);
+  function seed(address ifile, string hash1, string hash2) isSeeding(ifile) returns(bool res) {
+    seeders[msg.sender].push(hash1);
+    seeders[msg.sender].push(hash2);
     File(ifile).addSeeder(msg.sender);
+    return true;
   }
   
-  function unseed(address ifile) returns(bool res) {
+  function unseed(address ifile, string hash1, string hash2) returns(bool res) {
     if(msg.sender != File(ifile).seeders(msg.sender)) throw;
+    seeders[msg.sender].push(hash1);
+    seeders[msg.sender].push(hash2);
     File(ifile).removeSeeder(msg.sender);
   }
-  
-  function challenge() returns(bool res) {
-
-  }
-  
 }
 
 contract File {
-  address owner;
-  uint balance;
-  uint blockNum;
-  uint time;
-  bool setTime;
-  string fileHash1;
-  string fileHash2;
+  address public owner;
+  uint public balance;
+  uint public blockNum;
+  uint public time;
+  uint public challengeNum;
+  bool public setTime;
+  string public fileHash1;
+  string public fileHash2;
+  address[] confirmed;
   
   mapping (address => address) public seeders;
   
-  modifier setChallenge()
+  modifier isTime()
   {
     if (now < time + 1 minutes) throw;
-    time = now;
-    blockNum = block.number;
     _
   }
   
   modifier isChallengeable(address challenger)
   {
-    if (now < time + 1 minutes) throw;
-    if (setTime) time = now;
+    if (seeders[challenger] == 0x0) throw;
+    if (now > time + 1 minutes) {
+      time = now;
+      blockNum = block.number;      
+    }
     _
   }
   
-  function File(address s, uint v, string hash1, string hash2) {
-    owner = s;
-    balance = v;
+  function File(address _owner, uint _balance, string _fileHash1, string _fileHash2) {
+    owner = _owner;
+    balance = _balance;
+    blockNum = block.number;
     setTime = true;
     time = now;
-    fileHash1 = hash1;
-    fileHash2 = hash2;
+    challengeNum = 0;
+    fileHash1 = _fileHash1;
+    fileHash2 = _fileHash2;
+  }
+  
+  function setNewChallenge() isTime() {
+    time = now;
+    blockNum = block.number;
+    challengeNum++;
+    pay();
   }
   
   function addSeeder(address s) {
     seeders[s] = s;
   }
+  
   function removeSeeder(address s) {
     seeders[s] = 0x0;
-  }  
+  }
+  
+  function challenge() isChallengeable(msg.sender){
+    confirmed.push(msg.sender);
+  }
+  
+  function pay() {
+    for(uint i = 0; i < confirmed.length - 1; i++) {
+      confirmed[i].send(1337);
+      delete confirmed[i];
+    }
+  }
 }
