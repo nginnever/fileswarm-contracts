e80bcc69748e1d173c8d011f1d06fb999cc5f03b
nginnever <ginneversource@gmail.com>
2016-08-12 12:54:43 -0700

    mappings > arrays...

--- contracts/FileswarmManager.sol
+++ contracts/FileswarmManager.sol
@@ -1,11 +1,17 @@
 contract FileswarmManager {
   address public owner;
   address[] public files;
-  mapping (address => string[]) public seeders;
+  
+  struct IPFS {
+    string hash1;
+    string hash2;
+  }
+  
+  mapping (address => IPFS) public seeders;
 
   modifier isSeeding(address x)
   {
-    if(File(x).seeders(msg.sender) != 0x0) throw;
+    if(File(x).getSeederAddr(msg.sender) != 0x0) throw;
     _
   }
 
@@ -17,39 +23,64 @@ contract FileswarmManager {
     return files.length;
   }
   
-  function upload(string hash1, string hash2) returns(bool res) {
-    address f = new File(msg.sender, msg.value, hash1, hash2);
+  function getFile(uint i) public constant returns(address) {
+    return files[i];
+  }
+  
+  function createFile() returns(bool res) {
+    address f = new File(msg.sender, msg.value);
     files.push(f);
     return true;
   }
   
-  function seed(address ifile, string hash1, string hash2) isSeeding(ifile) returns(bool res) {
-    seeders[msg.sender].push(hash1);
-    seeders[msg.sender].push(hash2);
-    File(ifile).addSeeder(msg.sender);
+  function seed(address ifile, string _hash1, string _hash2) isSeeding(ifile) returns(bool res) {
+    IPFS entry = seeders[msg.sender];
+    entry.hash1 = _hash1;
+    entry.hash2 = _hash2;
+    File(ifile).addSeeder(msg.sender, _hash1, _hash2);
     return true;
   }
   
-  function unseed(address ifile, string hash1, string hash2) returns(bool res) {
-    if(msg.sender != File(ifile).seeders(msg.sender)) throw;
-    seeders[msg.sender].push(hash1);
-    seeders[msg.sender].push(hash2);
+  function unseed(address ifile, string _hash1, string _hash2) returns(bool res) {
+    if(msg.sender != ifile) throw;
+    IPFS entry = seeders[msg.sender];
+    entry.hash1 = _hash1;
+    entry.hash2 = _hash2;
     File(ifile).removeSeeder(msg.sender);
   }
 }
 
 contract File {
+    
+  uint public test = 0;
+  
   address public owner;
   uint public balance;
   uint public blockNum;
   uint public time;
   uint public challengeNum;
+  uint public numChunks;
   bool public setTime;
-  string public fileHash1;
-  string public fileHash2;
-  address[] confirmed;
+  string public challengeHash1;
+  string public challengeHash2;
+  address[] public confirmed;
+  uint amt = 14;
   
-  mapping (address => address) public seeders;
+  struct IPFS {
+    string hash1;
+    string hash2;
+    address seeder;
+  }
+  
+  struct Chunk {
+    string hash1;
+    string hash2;
+    string challenge1;
+    string challenge2;
+  }
+  
+  mapping (uint => Chunk) public chunks;
+  mapping (address => IPFS) public seeders;
   
   modifier isTime()
   {
@@ -59,23 +90,49 @@ contract File {
   
   modifier isChallengeable(address challenger)
   {
-    if (seeders[challenger] == 0x0) throw;
-    if (now > time + 1 minutes) {
-      time = now;
-      blockNum = block.number;      
-    }
+    if (seeders[challenger].seeder == 0x0) throw;
     _
   }
   
-  function File(address _owner, uint _balance, string _fileHash1, string _fileHash2) {
+  function File(address _owner, uint _balance) {
     owner = _owner;
     balance = _balance;
     blockNum = block.number;
     setTime = true;
     time = now;
     challengeNum = 0;
-    fileHash1 = _fileHash1;
-    fileHash2 = _fileHash2;
+  }
+  
+  function setNumChunks(uint num) {
+    numChunks = num;
+  }
+  
+  function addChunk(uint index, string _hash1, string _hash2) {
+    Chunk memory entry;
+    entry.hash1 = _hash1;
+    entry.hash2 = _hash2;
+    chunks[index] = entry;
+  }
+  
+  // Since sol can't return structs externally,
+  // we have a getter for each member.
+  //function getChunk(uint i) internal constant returns(IPFS) {
+  //  IPFS chunk = chunks[i];
+  //  return chunk;
+  //}
+  
+  function getChunk(uint i) public returns (string _hash1, string _hash2) {
+    Chunk temp = chunks[i];
+    _hash1 = temp.hash1;
+    _hash2 = temp.hash2;
+  }
+  
+  function getConfirmedCount() public constant returns(uint) {
+    return confirmed.length;
+  }
+  
+  function getSeederAddr(address a) public constant returns(address) {
+    return seeders[a].seeder;
   }
   
   function setNewChallenge() isTime() {
@@ -85,22 +142,48 @@ contract File {
     pay();
   }
   
-  function addSeeder(address s) {
-    seeders[s] = s;
+  function addSeeder(address s, string _hash1, string _hash2) {
+    seeders[s].seeder = s;
+    seeders[s].hash2 = _hash1;
+    seeders[s].hash2 = _hash1;
   }
   
   function removeSeeder(address s) {
-    seeders[s] = 0x0;
+    seeders[s].seeder = 0x0;
+    seeders[s].hash1 = "";
+    seeders[s].hash2 = "";
   }
   
-  function challenge() isChallengeable(msg.sender){
-    confirmed.push(msg.sender);
+  function challenge(string _hash1, string _hash2) isChallengeable(msg.sender){
+    IPFS s = seeders[msg.sender];
+    if(validate()) {
+      confirmed.push(msg.sender);
+    }
+  }
+  
+  function validate() internal returns(bool) {
+    
   }
   
   function pay() {
-    for(uint i = 0; i < confirmed.length - 1; i++) {
-      confirmed[i].send(1337);
+    for(uint i = 0; i < confirmed.length; i++) {
+      test++;
+      balance - amt;
+      bool tests = confirmed[i].send(amt);
       delete confirmed[i];
     }
+    confirmed.length = 0;
+  }
+  
+  function stringsEqual(string storage _a, string memory _b) internal returns (bool) {
+	bytes storage a = bytes(_a);
+	bytes memory b = bytes(_b);
+	if (a.length != b.length)
+	  return false;
+	// @todo unroll this loop
+	for (uint i = 0; i < a.length; i ++)
+		if (a[i] != b[i])
+			return false;
+	return true;
   }
 }
